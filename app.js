7284607d603ecf9dd8d387efe687968224c87b92
MADMAX-9999 <marcin.wozniak.studio@gmail.com>
2025-05-21 20:59:03 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StrategyCalculator from './ffcalc.js';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StrategyCalculator />
+    </div>
+  );
+}
+
+export default App;
